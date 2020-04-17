fd5857618727dab0f66f9b5328b906fa47447bb4
TheBigCheese <13ros27@kesgmail.net>
2020-04-17 17:08:44 +0100

    Added the ability for a point to move along at a constant velocity

--- object.py
+++ object.py
@@ -1,4 +1,6 @@
-"""The class structures to hold information about items in space."""
+"""The classes to hold information about items in space and space itself."""
+
+TIMESTEP = 0.01
 
 
 class Point:
@@ -9,3 +11,11 @@ class Point:
         self.mass = mass
         self.pos = pos
         self.vel = vel
+
+    def step_pos(self, timestep=TIMESTEP):
+        """Step the position forward according to the points velocity."""
+        self.pos = self.pos + self.vel*TIMESTEP
+
+    def step(self, timestep=TIMESTEP):
+        """Step the point forward one timestep."""
+        self.step_pos(timestep=timestep)
